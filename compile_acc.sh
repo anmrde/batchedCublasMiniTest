9b3636ac76d98e0fe70bceb71ced382a47891724
Andreas Mueller <andreas.mueller@ecmwf.int>
2022-07-07 22:41:16 +0200

    corrected compile scripts

--- compile_acc.sh
+++ compile_acc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-nvcc cublasSgemmBatched.cu -dc -ccbin nvc++
+nvcc cublasSgemmBatched.cu -dc
 nvfortran -c cublas_mod.F90
 nvfortran -Dacc -o cublas-batch.exe cublas-batch.F90 cublas_mod.o cublasSgemmBatched.o -acc -ta=nvidia -Minfo -Mcuda
